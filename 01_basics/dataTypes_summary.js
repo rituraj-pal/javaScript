0f165cb7a7336e17f9a971ff2a887c3a8332d7bf
Hrituraj <121292039+rituraj-pal@users.noreply.github.com>
2025-04-14 03:41:29 +0000

    dataTypes Summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,34 @@
+// primitive dataTypes
+// 7 types: number ,String ,null, BigInt, undefined,symbol,boolean
+
+const score=78
+const scoreValue=100.78
+
+const loggedIn=false;
+const outSideTemp=null;
+let userEmail;
+
+const id= Symbol('123')
+const anotherId=('123')
+
+console.log(id==anotherId)
+
+const bigNumber=1234834678523645435647547n
+
+// reference (Non-primitive)
+//  array ,object ,function
+
+const heros= [ "bheem" ,"raju" ,"nobita"]
+
+let myObj={
+ name :"rituraj",
+    age :18,
+}
+
+const myFunction=function(){
+    console.log("hello World")
+}
+
+console.log(heros)
+
+
